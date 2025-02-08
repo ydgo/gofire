dad92753b23ec59a7b0875a10014da650eb3cd6f
yudong <dong.yu@miaoyun.io>
2025-02-08 10:38:02 +0800

    refactor(processor): 重构 processor

--- processor/split.go
+++ processor/split.go
@@ -4,9 +4,7 @@ import (
 	"errors"
 	"gofire/component"
 	"gofire/event"
-	"gofire/metrics"
 	"strings"
-	"time"
 )
 
 func init() {
@@ -14,25 +12,19 @@ func init() {
 }
 
 type SplitProcessor struct {
-	pipeline string
-	sep      string
-	metrics  *metrics.ProcessorBasicMetrics
+	sep string
 }
 
-func NewSplitProcessor(pipeName string, cfg map[string]interface{}, collector *metrics.Collector) (component.Processor, error) {
-	if sep, ok := cfg["separator"].(string); ok {
-		return &SplitProcessor{sep: sep, pipeline: pipeName, metrics: collector.ProcessorBasicMetrics()}, nil
+func NewSplitProcessor(opts component.ProcessorOpts) (component.Processor, error) {
+	if sep, ok := opts.Setting["separator"].(string); ok {
+		return &SplitProcessor{sep: sep}, nil
 	}
 	return nil, errors.New("separator not found in config")
 }
 
 func (p *SplitProcessor) Process(events ...*event.Event) ([]*event.Event, error) {
-	start := time.Now()
 	data := make([]*event.Event, 0)
 	for _, evt := range events {
-		// todo 每个 pipeline 可能会有多个同类型的 processor
-		p.metrics.IncTotal(p.pipeline, "split")
-
 		raws := strings.Split(evt.Message, p.sep)
 		for _, raw := range raws {
 			newEvent := evt.Clone()
@@ -42,6 +34,5 @@ func (p *SplitProcessor) Process(events ...*event.Event) ([]*event.Event, error)
 		// 丢弃并释放旧的 event
 		evt.Release()
 	}
-	p.metrics.AddProcessDuration(p.pipeline, "split", time.Since(start))
 	return data, nil
 }
